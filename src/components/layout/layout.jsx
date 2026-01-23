c79e79f233bcd1e3d53f2a5e9db4292acf5c7c0a
SherazAhmad17 <sherazahmad7434@gmail.com>
2026-01-23 19:56:05 +0500

    adding matrialUl

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,12 +2,12 @@ import { Outlet } from "react-router-dom";
 import { FooterPage } from "../ui/footer";
 import HeaderPage from "../ui/header.jsx";
 
-export function LayoutPage(){
-    return (
-        <>
-          <HeaderPage/>
-       <Outlet/>
-       <FooterPage/>
-        </>
-    );
+export function LayoutPage() {
+  return (
+    <>
+      <HeaderPage />
+      <Outlet />
+      <FooterPage />
+    </>
+  );
 }
