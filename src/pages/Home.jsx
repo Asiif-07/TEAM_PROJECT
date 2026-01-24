468907bd890ab7ac4cef747a19ce0a352912bbeb
Asiif-07 <asifaligumato@gmail.com>
2026-01-24 19:03:38 +0500

    fix the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,9 @@
-import Banner from "../components/ui/Banner.jsx";
+// import Banner from "../components/ui/Banner.jsx";
 
 function HomePage() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold text-center py-8">Home Page</h1>
-            <Banner
-                title="lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-                text="lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-                image="https://via.placeholder.com/150"
-                buttonText="lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-            />
-        </div>
+       
+            <h1>Home Page</h1>
     );
 }
 
