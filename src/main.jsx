e976c9e7044f86e9f2587592a309536dfaf88e6f
Asiif-07 <asifaligumato@gmail.com>
2026-01-26 05:20:39 +0500

    corrct names

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Header from "./components/ui/header";
-// import ComingSoon from "./components/pages/ComingSoon";
-
+import { RouterProvider } from "react-router-dom";
+import router from "./router.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CssBaseline />
-      <Header />
-{/* 
-      <Routes>
-        <Route path="/" element={<ComingSoon />} />
-        <Route path="/how-it-work" element={<ComingSoon />} />
-        <Route path="/about" element={<ComingSoon />} />
-        <Route path="/contact" element={<ComingSoon />} />
-        <Route path="/pricing" element={<ComingSoon />} />
-        <Route path="/blog" element={<ComingSoon />} />
-      </Routes> */}
-    </BrowserRouter>
+    <CssBaseline />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
