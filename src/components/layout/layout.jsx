96dfb04990e67823bde5953d599288bcfd14d05e
fareehakarim <farihakarim100@gmail.com>
2026-01-26 18:02:01 +0500

    add companies section

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../ui/footer.jsx";
 import Header from "../ui/header.jsx";
+import Companies from "../ui/Companies.jsx";
+
 
 export function LayoutPage() {
   return (
     <>
       <Header />
       <Outlet />
+       <Companies/>
       <Footer />
+     
     </>
   );
 }
