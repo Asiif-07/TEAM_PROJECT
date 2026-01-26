5da5c0261fbf362d8cababc26dd17c68e1ec5548
fareehakarim <farihakarim100@gmail.com>
2026-01-26 18:48:32 +0500

    pull main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 // import Banner from "../components/ui/Banner.jsx";
 
 import Companies from "../components/ui/Companies";
-
+// import Services from "../components/ui/Services.jsx";
 function HomePage() {
     return (
        
         <>
             <h1>Home Page</h1>
             <Companies/>
+            {/* <Services/> */}
             </>
     );
 }
