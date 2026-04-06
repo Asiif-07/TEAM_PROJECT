e7994c24c0e15c503c80ee06c313983b0a27f413
Sheraz Ahmad <sherazahmad7434@gmail.com>
2026-04-06 13:06:24 +0500

    implemented cloudinary

--- server/router/cv.route.js
+++ server/router/cv.route.js
@@ -3,12 +3,17 @@ import { CreateCv, updateCv, getAllCvs,SingleCv,deleteCv } from "../controller/c
 import  validate  from '../middleWare/validate.js'
 import  {CvSchema,updateCvSchema} from '../schemas/cv.schema.js'
 import authMiddleWare from "../middleWare/authMiddleWare.js";
+import parseJsonFields from "../middleWare/parserJsonFields.js";
+import { imageMulter } from "../middleWare/imageMulter.middleWare.js";
 
 const cvRouter = Router();
 
 cvRouter.use(authMiddleWare)
 
-cvRouter.route("/cv").post(validate(CvSchema), CreateCv).get(getAllCvs)
+const uploadProfile = imageMulter(5,["image/jpeg","image/png","image/jpg"])
+
+
+cvRouter.route("/cv").post(uploadProfile.single("profileImage"),parseJsonFields(["education", "skills", "projects", "experience"]),validate(CvSchema), CreateCv).get(getAllCvs)
 cvRouter.route("/cv/:id").put(validate(updateCvSchema) , updateCv).get(SingleCv).delete(deleteCv)
 
 export default cvRouter
